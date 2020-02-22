0ec48652b623699b83a604f778887d4350d625b3
Vili Lipo <vili96lipo@gmail.com>
2020-02-22 20:39:42 +0200

    code for the test scenarios

--- controllers/content.js
+++ controllers/content.js
@@ -25,7 +25,11 @@ contentRouter.post('/', async (request, response) => {
     const jsonData = newPost.getJsonRepresentation();
     const moderationresult = await moderationSaga(jsonData);
     if (moderationresult) {
-      response.status(200).json({ status: 200 });
+      const [postAfterModeration] = await Post.findAll({
+        include: [Comment],
+        where: { id: newPost.id },
+      });
+      response.status(200).json(postAfterModeration);
     } else {
       response.status(401).json({ error: 'Post did not pass automatic modaration' });
     }
@@ -43,11 +47,14 @@ contentRouter.post('/comment', async (request, response) => {
     const post = await Post.findOne({ where: { id: postId }, include: [Comment] });
     await newComment.setPost(post);
     const jsonData = newComment.getJsonRepresentation();
-    await moderationSaga(jsonData);
-    response.status(200).json({ status: 200 });
+    const moderationResult = await moderationSaga(jsonData);
+    if (moderationResult) {
+      return response.status(200).json(newComment);
+    }
+    return response.status(401).json({ error: 'Content did not pass moderation' });
   } catch (error) {
     console.error(error);
-    response.status(500).json({ status: 500 });
+    return response.status(500).json({ status: 500 });
   }
 });
 
